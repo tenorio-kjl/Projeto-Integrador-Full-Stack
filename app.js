2804cae117c0efb35f47ff6be4813a41e65fc9a6
tenorio-kjl <kleitson@gmail.com>
2025-09-29 19:52:22 -0400

    alter all

--- app.js
+++
@@ -1,9 +0,0 @@
-const http = require(‘http’);
-const server = http.createServer((req, res) => {
-res.writeHead(200, {‘Content-Type’: ‘text/plain’});
- res.end(‘Olá, Mundo!’)
-});
-const PORT = 3000;
-server.listen(PORT, () => {
-console.log(`Servidor rodando em http://localhost:${PORT}/`);
-});
